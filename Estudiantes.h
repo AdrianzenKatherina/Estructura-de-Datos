c73c6ee285e5a10bb3388c8af271eb861515ac68
AdrianzenKatherina <159058526+AdrianzenKatherina@users.noreply.github.com>
2024-03-04 15:25:56 -0400

    Estudiantes.h

---
+++ Estudiantes.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Estudiante
+{ private:
+	string nombre; 
+	int edad;
+	string materia;
+	float calificacion;
+	
+public:
+	Estudiante();
+	~Estudiante();
+	
+	void set_nombre(string n);
+	string get_nombre();
+
+};
